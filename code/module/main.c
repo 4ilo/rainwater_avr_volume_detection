2c52bfa32d5e47d6f0522fcc5c8f288af05bea0f
Olivier <oliviervandeneede@hotmail.com>
2020-08-11 17:48:59 +0200

    Add lcd driver

--- code/module/main.c
+++ code/module/main.c
@@ -2,17 +2,43 @@
 #include <util/delay.h>
 
 #include "gpio/gpio.h"
+#include "HD44780/lcd.h"
 
 
 int main(void)
 {
     gpio_init(PORT_B, PIN_0, GPIO_OUT);
 
-    while (1) {
-        gpio_write(PORT_B, PIN_0, 1);
-        _delay_ms(1000);
+    gpio_init(PORT_C, 0, GPIO_OUT);
+    gpio_init(PORT_C, 1, GPIO_OUT);
+    gpio_init(PORT_C, 2, GPIO_OUT);
+    gpio_init(PORT_C, 3, GPIO_OUT);
+    gpio_init(PORT_C, 4, GPIO_OUT);
+    gpio_init(PORT_C, 5, GPIO_OUT);
 
-        gpio_write(PORT_B, PIN_0, 0);
+    gpio_init(PORT_C, 0, GPIO_OUT);
+    gpio_init(PORT_D, 7, GPIO_OUT);
+    gpio_init(PORT_D, 6, GPIO_OUT);
+    gpio_init(PORT_D, 5, GPIO_OUT);
+
+    //Lcd_PortType ports[] = {PORT_B, PORT_D, PORT_D, PORT_D,
+    //                         PORT_C, PORT_C, PORT_C, PORT_C};
+    //Lcd_PinType pins[] = {0, 7, 6, 5,
+    //                        2, 3, 4, 5};
+    Lcd_PortType ports[] = {PORT_C, PORT_C, PORT_C, PORT_C};
+    Lcd_PinType pins[] = {2, 3, 4, 5};
+    Lcd_HandleTypeDef lcd;
+
+    lcd = Lcd_create(ports, pins, PORT_C, PIN_0, PORT_C, PIN_1, LCD_4_BIT_MODE);
+
+    Lcd_cursor(&lcd, 1, 0);
+    Lcd_string(&lcd, "Hello");
+
+    while (1) {
+//        gpio_write(PORT_B, PIN_0, 1);
         _delay_ms(1000);
+//
+//        gpio_write(PORT_B, PIN_0, 0);
+//        _delay_ms(1000);
     }
 }
--- code/module/src/HD44780/lcd.c
+++ code/module/src/HD44780/lcd.c
@@ -5,16 +5,20 @@
  *  Author: Olivier Van den Eede
  */
 
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
 #include "lcd.h"
 
-#define DELAY(X) HAL_Delay(X)
+#define DELAY(X) _delay_ms(X)
+#define VALIDATE_RET(X) (X == 0) ? 0 : -1
 
+#define MAX_BUFFER 11
 
 /************************************** Static declarations **************************************/
 
-static void lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data);
-static void lcd_write_command(Lcd_HandleTypeDef * lcd, uint8_t command);
-static void lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len);
+static int8_t lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t reg);
+static int8_t lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len);
 
 
 /************************************** Function definitions **************************************/
@@ -23,130 +27,129 @@ static void lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len);
  * Create new Lcd_HandleTypeDef and initialize the Lcd
  */
 Lcd_HandleTypeDef Lcd_create(
-		Lcd_PortType port[], Lcd_PinType pin[],
-		Lcd_PortType rs_port, Lcd_PinType rs_pin,
-		Lcd_PortType en_port, Lcd_PinType en_pin, Lcd_ModeTypeDef mode)
+    Lcd_PortType port[], Lcd_PinType pin[],
+    Lcd_PortType rs_port, Lcd_PinType rs_pin,
+    Lcd_PortType en_port, Lcd_PinType en_pin, Lcd_ModeTypeDef mode)
 {
-	Lcd_HandleTypeDef lcd;
+    Lcd_HandleTypeDef lcd;
 
-	lcd.mode = mode;
+    lcd.mode = mode;
 
-	lcd.en_pin = en_pin;
-	lcd.en_port = en_port;
+    lcd.en_pin = en_pin;
+    lcd.en_port = en_port;
 
-	lcd.rs_pin = rs_pin;
-	lcd.rs_port = rs_port;
+    lcd.rs_pin = rs_pin;
+    lcd.rs_port = rs_port;
 
-	lcd.data_pin = pin;
-	lcd.data_port = port;
+    lcd.data_pin = pin;
+    lcd.data_port = port;
 
-	Lcd_init(&lcd);
+    Lcd_init(&lcd);
 
-	return lcd;
+    return lcd;
 }
 
 /**
  * Initialize 16x2-lcd without cursor
  */
-void Lcd_init(Lcd_HandleTypeDef * lcd)
+int8_t Lcd_init(Lcd_HandleTypeDef * lcd)
 {
-	if(lcd->mode == LCD_4_BIT_MODE)
-	{
-			lcd_write_command(lcd, 0x33);
-			lcd_write_command(lcd, 0x32);
-			lcd_write_command(lcd, FUNCTION_SET | OPT_N);				// 4-bit mode
-	}
-	else
-		lcd_write_command(lcd, FUNCTION_SET | OPT_DL | OPT_N);
+    int8_t ret = 0;
 
+    if (lcd->mode == LCD_4_BIT_MODE) {
+        ret += lcd_write_data(lcd, 0x33, LCD_COMMAND_REG);
+        ret += lcd_write_data(lcd, 0x32, LCD_COMMAND_REG);
+        ret += lcd_write_data(lcd, FUNCTION_SET | OPT_N, LCD_COMMAND_REG);      // 4-bit mode
+    }
+    else {
+        ret += lcd_write_data(lcd, FUNCTION_SET | OPT_DL | OPT_N, LCD_COMMAND_REG);
+    }
 
-	lcd_write_command(lcd, CLEAR_DISPLAY);						// Clear screen
-	lcd_write_command(lcd, DISPLAY_ON_OFF_CONTROL | OPT_D);		// Lcd-on, cursor-off, no-blink
-	lcd_write_command(lcd, ENTRY_MODE_SET | OPT_INC);			// Increment cursor
+    ret += lcd_write_data(lcd, CLEAR_DISPLAY, LCD_COMMAND_REG);                      // Clear screen
+    ret += lcd_write_data(lcd, DISPLAY_ON_OFF_CONTROL | OPT_D, LCD_COMMAND_REG);     // Lcd-on, cursor-off, no-blink
+    ret += lcd_write_data(lcd, ENTRY_MODE_SET | OPT_INC, LCD_COMMAND_REG);           // Increment cursor
+
+    return VALIDATE_RET(ret);
 }
 
 /**
  * Write a number on the current position
  */
-void Lcd_int(Lcd_HandleTypeDef * lcd, int number)
+int8_t Lcd_int(Lcd_HandleTypeDef * lcd, int number)
 {
-	char buffer[11];
-	sprintf(buffer, "%d", number);
+    int8_t ret = 0;
+    char buffer[MAX_BUFFER];
 
-	Lcd_string(lcd, buffer);
+    ret += snprintf(buffer, MAX_BUFFER, "%d", number);
+    ret += Lcd_string(lcd, buffer);
+
+    return VALIDATE_RET(ret);
 }
 
 /**
  * Write a string on the current position
  */
-void Lcd_string(Lcd_HandleTypeDef * lcd, char * string)
+int8_t Lcd_string(Lcd_HandleTypeDef * lcd, char * string)
 {
-	for(uint8_t i = 0; i < strlen(string); i++)
-	{
-		lcd_write_data(lcd, string[i]);
-	}
+    int8_t ret = 0;
+
+    for(uint8_t i = 0; i < strlen(string); i++)
+    {
+        ret += lcd_write_data(lcd, string[i], LCD_DATA_REG);
+    }
+
+    return VALIDATE_RET(ret);
 }
 
 /**
  * Set the cursor position
  */
-void Lcd_cursor(Lcd_HandleTypeDef * lcd, uint8_t row, uint8_t col)
+int8_t Lcd_cursor(Lcd_HandleTypeDef * lcd, uint8_t row, uint8_t col)
 {
-	lcd_write_command(lcd, SET_DDRAM_ADDR | ((row * 0x40) + col));
+    return lcd_write_data(lcd, SET_DDRAM_ADDR | ((row * 0x40) + col), LCD_COMMAND_REG);
 }
 
 
 /************************************** Static function definition **************************************/
 
 /**
- * Write a byte to the command register
+ * Write a byte to the data or command register
  */
-void lcd_write_command(Lcd_HandleTypeDef * lcd, uint8_t command)
+int8_t lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t reg)
 {
-	HAL_GPIO_WritePin(lcd->rs_port, lcd->rs_pin, LCD_COMMAND_REG);		// Write to command register
+    int8_t ret = 0;
 
-	if(lcd->mode == LCD_4_BIT_MODE)
-	{
-		lcd_write(lcd, (command >> 4), LCD_NIB);
-		lcd_write(lcd, command & 0x0F, LCD_NIB);
-	}
-	else
-	{
-		lcd_write(lcd, command, LCD_BYTE);
-	}
+    ret = gpio_write(lcd->rs_port, lcd->rs_pin, reg);
 
+    if(lcd->mode == LCD_4_BIT_MODE)
+    {
+        ret += lcd_write(lcd, data >> 4, LCD_NIB);
+        ret += lcd_write(lcd, data & 0x0F, LCD_NIB);
+    }
+    else
+    {
+        ret += lcd_write(lcd, data, LCD_BYTE);
+    }
+
+    return VALIDATE_RET(ret);
 }
 
-/**
- * Write a byte to the data register
- */
-void lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data)
-{
-	HAL_GPIO_WritePin(lcd->rs_port, lcd->rs_pin, LCD_DATA_REG);			// Write to data register
-
-	if(lcd->mode == LCD_4_BIT_MODE)
-	{
-		lcd_write(lcd, data >> 4, LCD_NIB);
-		lcd_write(lcd, data & 0x0F, LCD_NIB);
-	}
-	else
-	{
-		lcd_write(lcd, data, LCD_BYTE);
-	}
-
-}
 
 /**
  * Set len bits on the bus and toggle the enable line
  */
-void lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len)
+int8_t lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len)
 {
-	for(uint8_t i = 0; i < len; i++)
-	{
-		HAL_GPIO_WritePin(lcd->data_port[i], lcd->data_pin[i], (data >> i) & 0x01);
-	}
+    uint8_t ret = 0;
 
-	HAL_GPIO_WritePin(lcd->en_port, lcd->en_pin, 1);
-	DELAY(1);
-	HAL_GPIO_WritePin(lcd->en_port, lcd->en_pin, 0); 		// Data receive on falling edge
+    for(uint8_t i = 0; i < len; i++)
+    {
+        ret += gpio_write(lcd->data_port[i], lcd->data_pin[i], (data >> i) & 0x01);
+    }
+
+    ret += gpio_write(lcd->en_port, lcd->en_pin, 1);
+    DELAY(10);
+    ret += gpio_write(lcd->en_port, lcd->en_pin, 0);         // Data receive on falling edge
+
+    return VALIDATE_RET(ret);
 }
